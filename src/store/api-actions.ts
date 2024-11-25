8d67e79e4ac34123fd3a076b75407ed9179c4e00
Ilia Kovalchuk <sckremyulock@gmail.com>
2024-11-25 23:35:36 +0500

    in progress

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -2,7 +2,8 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AppDispatch, State} from '../types/state.ts';
 import {AxiosInstance} from 'axios';
 import Offer from '../types/offer.ts';
-import {addOffers, setOffersDataLoadingStatus} from './action.ts';
+import {addOffers, requireAuthorization, setOffersDataLoadingStatus} from './action.ts';
+import {AuthorizationStatus} from "../enums.ts";
 
 export const fetchOffersAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -18,4 +19,33 @@ export const fetchOffersAction = createAsyncThunk<void, undefined, {
   }
 );
 
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      await api.get('/login');
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
+  }
+);
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async ({login: email, password}, {dispatch, extra: api}) => {
+    const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    dispatch(redirectToRoute(AppRoute.Result));
+  },
+);
 
